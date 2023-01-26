a3f4a46bb25d683978be6f6b8793f76c487821d9
Seiichi Yonezawa <nzwsch@gmail.com>
2023-01-27 06:07:47 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { InteractionResponseType, verifyKey } from 'discord-interactions';
+
 /**
  * Welcome to Cloudflare Workers! This is your first worker.
  *
@@ -17,6 +19,9 @@ export interface Env {
   //
   // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
   // MY_BUCKET: R2Bucket;
+  DISCORD_APPLICATION_ID: string
+  DISCORD_PUBLIC_KEY: string
+  DISCORD_TOKEN: string
 }
 
 export default {
@@ -25,6 +30,34 @@ export default {
     env: Env,
     ctx: ExecutionContext
   ): Promise<Response> {
-    return new Response("Hello World!");
+    const rawBody = await request.text()
+    const signature = request.headers.get('X-Signature-Ed25519')
+    const timestamp = request.headers.get('X-Signature-Timestamp')
+    const pubKey = env.DISCORD_PUBLIC_KEY
+
+    if (signature == null || timestamp == null) {
+      return new Response('Sorry, you have supplied an invalid key.', {
+        status: 403
+      })
+    }
+
+    const isValidRequest = verifyKey(rawBody, signature, timestamp, pubKey)
+
+    if (!isValidRequest) {
+      return new Response('Bad request signature', {
+        status: 401
+      })
+    }
+
+    return new Response(
+      JSON.stringify({
+        type: InteractionResponseType.PONG
+      }),
+      {
+        headers: {
+          'content-type': 'application/json;charset=UTF-8'
+        }
+      }
+    );
   },
 };
